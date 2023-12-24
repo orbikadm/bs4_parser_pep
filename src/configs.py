49e56017d6ef807d4057e90af02267d2145c2145
dem507 <orbikadm@gmail.com>
2023-12-24 23:21:55 +1000

    Убрать последние 2 таблицы (резервный pep и людей)

--- src/configs.py
+++ src/configs.py
@@ -1,12 +1,11 @@
 import argparse
 import logging
 
-from constants import BASE_DIR
 from logging.handlers import RotatingFileHandler
+from constants import BASE_DIR
 
 
 LOG_FORMAT = '"%(asctime)s - [%(levelname)s] - %(message)s"'
-# Указываем формат времени.
 DT_FORMAT = '%d.%m.%Y %H:%M:%S'
 
 
@@ -38,22 +37,14 @@ def configure_argument_parser(available_modes):
 def configure_logging():
     log_dir = BASE_DIR / 'logs'
     log_dir.mkdir(exist_ok=True)
-    # Отсюда начинается новый код!
-    # Получение абсолютного пути до файла с логами.
     log_file = log_dir / 'parser.log'
 
-    # Инициализация хендлера с ротацией логов.
-    # Максимальный объём одного файла — десять в шестой степени байт (10**6), 
-    # максимальное количество файлов с логами — 5.
     rotating_handler = RotatingFileHandler(
         log_file, maxBytes=10 ** 6, backupCount=5, encoding='utf-8'
     )
-    # Базовая настройка логирования basicConfig.
     logging.basicConfig(
         datefmt=DT_FORMAT,
         format=LOG_FORMAT,
-        # Уровень записи логов.
         level=logging.INFO,
-        # Вывод логов в терминал.
         handlers=(rotating_handler, logging.StreamHandler())
     )
--- src/pep.py
+++ src/pep.py
@@ -2,9 +2,8 @@ from bs4 import BeautifulSoup
 import requests_cache
 import re
 from urllib.parse import urljoin
-from time import sleep
 from tqdm import tqdm
-
+from collections import Counter
 
 PEP_DOC_URL = 'https://peps.python.org/'
 EXPECTED_STATUS = {
@@ -17,22 +16,20 @@ EXPECTED_STATUS = {
     'W': ('Withdrawn',),
     '': ('Draft', 'Active'),
 }
+count_statuses = Counter()
+
 
 def get_pep_info(url):
-    # url = 'https://peps.python.org/pep-0249/' # Final
-    # url = 'https://peps.python.org/pep-0430/' # Final
-    # url = 'https://peps.python.org/pep-0708/' # Provisional
-    # url = 'https://peps.python.org/pep-0458/' # Accepted
     session = requests_cache.CachedSession()
     response = session.get(url)
     response.encoding = 'utf-8'
     soup = BeautifulSoup(response.text, features='lxml')
-    status = soup.find('abbr').text
-    print(status)
+    return soup.find('abbr').text
 
 
-# def validate_tr():
-
+# def validate_tr(tag):
+#     return tag.find('a', string=re.compile(r'\d{1,4}'))
+# href=re.compile("elsie"), id='link1'
 
 def pep():
     session = requests_cache.CachedSession()
@@ -41,37 +38,49 @@ def pep():
     soup = BeautifulSoup(response.text, features='lxml')
 
 
-
-
-
-
-
-### РАБОЧИЙ ВАРИАНТ
-    # tr_tags = soup.find_all('tr')
+    # tr_tags = soup.find_all(validate_tr)
     # for tr_tag in tr_tags:
-    #     abbr_tag = tr_tag.find('abbr')
-    #     if abbr_tag:
-    #         pep_status_in_list = abbr_tag.text
-    #     else:
+    #     if not tr_tag.find('a'):
     #         continue
-    #     href_attr = tr_tag.find('a', string=re.compile(r'\d{1,4}'))
-    #     if href_attr:
-    #         pep_url = urljoin(PEP_DOC_URL, href_attr['href'])
-    #     else:
-    #         continue
-    #
-    #     print(pep_status_in_list, pep_url)
-### РАБОЧИЙ ВАРИАНТ
+    # print(tr_tags)
 
-        # for url in pep_urls:
-        #     page_url = urljoin(PEP_DOc_URL, url['href'])
-        #     print(page_url)
-        #     sleep(1)
-        #     get_pep_info(page_url)
+
+### РАБОЧИЙ ВАРИАНТ
+    tr_tags = soup.find_all('tr')
+    for tr_tag in tqdm(tr_tags, desc='Пошла жара'):
+        if not tr_tag.find('a'):
+            continue
+        abbr_tag = tr_tag.find('abbr')
+        preview_status = ''
+        if abbr_tag:
+            preview_status = abbr_tag.text[1:]
+        href_attr = tr_tag.find('a', string=re.compile(r'\d{1,4}'))
+        pep_url = urljoin(PEP_DOC_URL, href_attr['href'])
+
+        pep_status = get_pep_info(pep_url)
+
+        if pep_status not in EXPECTED_STATUS[preview_status]:
+            # Здесь будет логгер
+            print(f"""
+Несовпадающие статусы:
+{pep_url} 
+Статус в карточке: {pep_status} 
+Ожидаемые статусы: {EXPECTED_STATUS[preview_status]}
+""")
+
+
+
+        count_statuses[pep_status] += 1
+        print(pep_status, preview_status, pep_url)
+
+    results = [('Статус', 'Количество')]
+    results.extend(count_statuses.items())
+    all_pep = sum(count_statuses.values())
+    results.append(('Total', all_pep))
+    print('RESULT: ', results)
+
+
 
 
 if __name__ == '__main__':
     pep()
-    # url = 'https://peps.python.org/pep-8105/'
-    #
-    # get_pep_info(url)
